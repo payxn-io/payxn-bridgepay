0f7b23085e7c0ef81a8a78e042cea89cfafbaeca
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-03 02:50:12 -0800

    🎭 Add function validateParameters

--- scripts/WatchForBridges.ts
+++ scripts/WatchForBridges.ts
@@ -10,3 +10,26 @@ dotenv.config();
 
 const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 const minterPrivateKey = process.env.PRIVATE_KEY || "";
+
+function validateParameters(parameters: string[]) {
+    if (!parameters || parameters.length < 4)
+      throw new Error("Parameters not provided");
+  
+    const sourceNetwork = parameters[0];
+    if (!sourceNetwork) throw new Error("Source network not provided");
+  
+    const sourceTokenAddress = parameters[1] as `0x${string}`;
+    if (!sourceTokenAddress) throw new Error("Source contract address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(sourceTokenAddress))
+      throw new Error("Invalid source contract address");
+  
+    const destinationNetwork = parameters[2];
+    if (!destinationNetwork) throw new Error("Destination network not provided");
+  
+    const destinationTokenAddress = parameters[3] as `0x${string}`;
+    if (!destinationTokenAddress) throw new Error("Destination contract address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(destinationTokenAddress))
+      throw new Error("Invalid destination contract address");
+  
+    return { sourceTokenAddress, sourceNetwork, destinationTokenAddress, destinationNetwork };
+  }
