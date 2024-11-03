a3c6cf4125ebcc76bb9f7593f4356b90cf97e20d
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-02 20:01:09 -0700

    📢 Added event Bridge

--- contracts/BridgeableToken.sol
+++ contracts/BridgeableToken.sol
@@ -6,5 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract BridgeableToken is ERC20, ERC20Burnable, AccessControl {
+    event Bridge(address indexed owner, uint256 indexed amount);
 
 }
