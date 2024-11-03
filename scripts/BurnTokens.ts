23e7881daff33a559e544a532e13532653e98baa
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-03 02:52:09 -0800

    🎭 Create BurnTokens script and add imports

---
+++ scripts/BurnTokens.ts
@@ -0,0 +1,9 @@
+// npx ts-node --files ./scripts/BurnTokens.ts CONTRACT_ADDRESS AMOUNT NETWORK
+
+import { createPublicClient, http, createWalletClient } from "viem";
+
+import { privateKeyToAccount } from "viem/accounts";
+import { baseSepolia, sepolia } from "viem/chains";
+import { abi } from "../artifacts/contracts/BridgeableToken.sol/BridgeableToken.json";
+import * as dotenv from "dotenv";
+dotenv.config();
