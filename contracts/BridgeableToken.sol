63bb314aea472a33b348a6f89d22c8afe0405f37
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-02 20:00:00 -0700

    📝 Created BridgeableToken contract

--- contracts/BridgeableToken.sol
+++ contracts/BridgeableToken.sol
@@ -4,3 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract BridgeableToken is ERC20, ERC20Burnable, AccessControl {
+
+}
