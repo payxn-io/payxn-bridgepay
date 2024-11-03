0fa4b2df197abbbe38db87c04a78048e8b433038
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-03 02:55:53 -0800

    🎭 Define constants

--- scripts/BridgeTokens.ts
+++ scripts/BridgeTokens.ts
@@ -7,3 +7,6 @@ import { baseSepolia, sepolia } from "viem/chains";
 import { abi } from "../artifacts/contracts/BridgeableToken.sol/BridgeableToken.json";
 import * as dotenv from "dotenv";
 dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const minterPrivateKey = process.env.PRIVATE_KEY || "";
