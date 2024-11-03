07c2acea3d980cd4a74576db709958048b87fd07
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-03 01:48:28 -0800

    🎨 Modified layout frontend

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "BridgePay - Payxn",
+  description: "Pay with USDC on one chain to another",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
