3e74559da97f0241eb4194c59d141641187bf0e5
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-11-03 02:49:27 -0800

    🎭 Create WatchForBridges script and add imports

---
+++ scripts/WatchForBridges.ts
@@ -0,0 +1,9 @@
+// npx ts-node --files ./scripts/WatchForBridges.ts SOURCE_NETWORK SOURCE_TOKEN_ADDRESS DESTINATION_NETWORK DESTINATION_TOKEN_ADDRESS
+
+import { createPublicClient, createWalletClient, http, parseAbiItem } from "viem";
+
+import { privateKeyToAccount } from "viem/accounts";
+import { baseSepolia, sepolia } from "viem/chains";
+import { abi } from "../artifacts/contracts/BridgeableToken.sol/BridgeableToken.json";
+import * as dotenv from "dotenv";
+dotenv.config();
